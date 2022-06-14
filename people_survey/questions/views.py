c0c982205096786b68b0698a402011e40c4f52a7
Ed Singleton <singletoned@gmail.com>
2022-06-14 13:59:42 +0100

    Add basic view and template

--- people_survey/questions/views.py
+++ people_survey/questions/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def question_view(request):
+    return render(
+        request,
+        template_name="questions/question.html",
+        context={},
+    )
+    
