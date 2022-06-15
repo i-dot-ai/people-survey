9e49f3294c2c41b9c7b0b0bb7a2a584eb69bd80d
Ed Singleton <singletoned@gmail.com>
2022-06-15 10:00:18 +0100

    Add forms to page

--- people_survey/questions/views.py
+++ people_survey/questions/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
+from . import forms
+
+forms_mapping = {
+'1': forms.Group1Form,
+'2': forms.Group2Form,
+}
+
 def question_view(request):
+    param = request.GET['form']
+    form = forms_mapping.get(param)
     return render(
         request,
         template_name="questions/question.html",
-        context={},
+        context={'form': form},
     )
     
