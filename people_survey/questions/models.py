179ced69895ddfeb8fd7c4b9e022906b61cf71fd
Ed Singleton <singletoned@gmail.com>
2022-06-21 12:05:41 +0100

    Add User and Answer models

--- people_survey/questions/models.py
+++ people_survey/questions/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, related_name="answers", on_delete=models.PROTECT)
