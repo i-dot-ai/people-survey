ceebf1d45872a728ee1f066a2194969708853c4f
Ed Singleton <singletoned@gmail.com>
2022-08-19 14:04:07 +0100

    Add some basic page views

--- people_survey/survey/views.py
+++ people_survey/survey/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
 
+@require_http_methods(["GET"])
 def index_view(request):
     return render(
         request,
         template_name="index.html",
+        context={"request": request},
+    )
+
+
+@require_http_methods(["GET"])
+def builder_view(request):
+    return render(
+        request,
+        template_name="builder.html",
+        context={"request": request},
+    )
+
+
+@require_http_methods(["GET"])
+def survey_view(request):
+    return render(
+        request,
+        template_name="survey.html",
         context={"request": request},
     )
