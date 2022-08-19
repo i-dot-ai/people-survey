ce9aa4e43674f7e9762fdd29ec94904271a48342
Ed Singleton <singletoned@gmail.com>
2022-08-19 14:40:09 +0100

    Add more api endpoints

--- people_survey/survey/views.py
+++ people_survey/survey/views.py
@@ -35,6 +35,31 @@ def survey_view(request):
     )
 
 
+@api.get("/survey")
+def api_builder_get(request):
+    if Survey.objects.count():
+        survey = Survey.objects.get(1).data
+    else:
+        survey = None
+    return survey
+
+
 @api.post("/survey")
 def api_builder_post(request, data: SurveySchema):
-    return
+    survey = Survey.objects.update_or_create(id=1, data=str(data.data))
+    return None
+
+
+@api.get("/answer")
+def api_answer_get(request):
+    if Answer.objects.count():
+        answer = Answer.objects.first().data
+    else:
+        answer = None
+    return answer
+
+
+@api.post("/answer")
+def api_answer_post(request, data: AnswerSchema):
+    answer = Answer.objects.update_or_create(data=str(data.data))
+    return None
