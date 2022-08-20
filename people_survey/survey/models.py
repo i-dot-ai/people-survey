dd88c16172f7cd8f0c4bddbff07f90b3582f7231
Ed Singleton <singletoned@gmail.com>
2022-08-20 15:56:21 +0100

    Use django-use-email-as-username in model

--- people_survey/survey/models.py
+++ people_survey/survey/models.py
@@ -1,8 +1,10 @@
-from cuser.models import AbstractCUser
 from django.db import models
+from django_use_email_as_username.models import BaseUser, BaseUserManager
 
 
-class User(AbstractCUser):
+class User(BaseUser):
+    objects = BaseUserManager()
+
     def save(self, *args, **kwargs):
         self.email = self.email.lower()
         super().save(*args, **kwargs)
