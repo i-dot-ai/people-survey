e7a5432ba83de919bb04c3595039c712862d2120
Ed Singleton <singletoned@gmail.com>
2022-08-20 20:15:46 +0100

    Add urls for builder and survey

--- people_survey/urls.py
+++ people_survey/urls.py
@@ -6,6 +6,8 @@ from people_survey.survey import views
 urlpatterns = [
     path("", views.index_view, name="index"),
     path("home/", views.homepage_view, name="homepage"),
+    path("builder/", views.builder_view, name="builder"),
+    path("survey/", views.survey_view, name="survey"),
     path("admin/", admin.site.urls),
     path("api/", views.api.urls),
     path("accounts/", include("allauth.urls")),
