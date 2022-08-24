c401a692b5ff03038fd0f704a1e79e738bea76d6
Ed Singleton <singletoned@gmail.com>
2022-08-24 08:05:41 +0100

    Update builder.js

--- static/builder.js
+++ static/builder.js
@@ -1,16 +1,43 @@
 const creatorOptions = {
-    showLogicTab: true,
-    isAutoSave: true
+  showLogicTab: true,
+  isAutoSave: true
 };
 
-const creator = new SurveyCreator.SurveyCreator(creatorOptions);
+function loadSurvey(data) {
+
+  const creator = new SurveyCreator.SurveyCreator(creatorOptions);
+
+  creator.JSON = data
+
+  creator.saveSurveyFunc = (saveNo, callback) => {
+    saveSurveyJson(
+      "http://localhost:8008/api/survey",
+      creator.JSON,
+      saveNo,
+      callback
+    );
+  };
+
+  creator.render("surveyCreator");
+
+};
+
+function saveSurveyJson(url, json, saveNo, callback) {
+  const request = new XMLHttpRequest();
+  request.open('POST', url);
+  request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  request.addEventListener('load', () => {
+    callback(saveNo, true);
+  });
+  request.addEventListener('error', () => {
+    callback(saveNo, false);
+  });
+  request.send(JSON.stringify({'data': json}));
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-    creator.render("surveyCreator");
+
+  fetch('http://localhost:8008/api/survey')
+    .then((response) => response.json())
+    .then((data) => loadSurvey(data['data']));
 });
-
-creator.saveSurveyFunc = (saveNo, callback) => {
-    window.localStorage.setItem("survey-json", creator.text);
-    callback(saveNo, true);
-
-};
